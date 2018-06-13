b1051d458d094c890b829b0534b93c6692a0185d
MrSmoke <syco711@gmail.com>
2018-06-13 15:49:29 +1000

    Initial code commit

---
+++ ClassLibrary1/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿namespace ClassLibrary1.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class HomeController : Controller
+    {
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("error")]
+        public IActionResult Error()
+        {
+            return Ok("Error page");
+        }
+    }
+}
---
+++ ClassLibrary1/ServiceExtensions.cs
@@ -0,0 +1,29 @@
+﻿namespace ClassLibrary1
+{
+    using System.Reflection;
+    using Microsoft.AspNetCore.Mvc.Razor;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.FileProviders;
+
+    public static class ServiceExtensions
+    {
+        public static void AddWeb(this IServiceCollection services, IMvcBuilder mvcBuilder)
+        {
+            var currentAssembly = typeof(ServiceExtensions).GetTypeInfo().Assembly;
+
+
+            //Add current assembly to application parts
+            mvcBuilder.AddApplicationPart(currentAssembly);
+
+            var fileProvider = new EmbeddedFileProvider(currentAssembly);
+
+            services.AddSingleton<IFileProvider>(fileProvider);
+
+            services.Configure<RazorViewEngineOptions>(options =>
+            {
+                options.FileProviders.Clear();
+                options.FileProviders.Add(fileProvider);
+            });
+        }
+    }
+}
